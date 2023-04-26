85c6df1b6c5b491531bc78d491bc569fb988624c
hawkup <spiritsquare@gmail.com>
2023-04-27 01:12:33 +0700

    Move to add tailwind on story

--- histoire.config.ts
+++ histoire.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'histoire'
 import { HstVue } from '@histoire/plugin-vue'
 
 export default defineConfig({
-  setupFile: './src/histoire-setup.js',
   plugins: [
     HstVue(),
   ],
---
+++ src/components/Count.story.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import Count from './Count.vue'
+import '../tailwind.css'
+</script>
+
+<template>
+  <Story>
+    <Count />
+  </Story>
+</template>
