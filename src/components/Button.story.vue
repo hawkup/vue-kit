044c6dfc5b3b01ccb2747887e442daaab555d7ec
hawkup <spiritsquare@gmail.com>
2023-05-02 23:02:21 +0700

    Add unocss

---
+++ src/components/Button.story.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import Button from './Button.vue'
+import 'virtual:uno.css'
+</script>
+
+<template>
+  <Story>
+    <Button />
+  </Story>
+</template>
---
+++ src/components/Button.vue
@@ -0,0 +1,3 @@
+<template>
+  <button type="button" class="text-xl c-yellow">click me</button>
+</template>
